09789b2b6f135a51c6f681d21d8bde2e879e5283
cbrecastle <christopher.castle@cbre.com>
2020-03-09 10:55:33 +0000

    added document viewer

--- CosmosWizzard.Web/Controllers/DatabaseDocument.cs
+++ CosmosWizzard.Web/Controllers/DatabaseDocument.cs
@@ -1,149 +1,64 @@
-﻿//namespace CosmosWizard.Web.Controllers
-//{
-//    using System.Linq;
-//    using System.Threading.Tasks;
-//    using CustomLogic.Database;
-//    using Microsoft.AspNetCore.Mvc;
-//    using Microsoft.EntityFrameworkCore;
+﻿namespace CosmosWizard.Web.Controllers
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+    using CustomLogic.Database;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
 
-//    public class DatabaseDocumentController : Controller
-//    {
-//        private readonly DatabaseMigratorContext _context;
+    public class DatabaseDocumentController : Controller
+    {
+        private readonly DatabaseMigratorContext _context;
 
-//        public DatabaseDocumentController(DatabaseMigratorContext context)
-//        {
-//            _context = context;
-//        }
+        public DatabaseDocumentController(DatabaseMigratorContext context)
+        {
+            _context = context;
+        }
 
-//        // GET: DocumentRecords
-//        public async Task<IActionResult> Index()
-//        {
-//            return View(await _context.DocumentRecords.ToListAsync());
-//        }
+        // GET: DocumentRecords
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.DocumentRecords.Where(c=>c.EntityType == "Action").ToListAsync());
+        }
 
-//        // GET: DocumentRecords/Details/5
-//        public async Task<IActionResult> Details(int? id)
-//        {
-//            if (id == null)
-//            {
-//                return NotFound();
-//            }
+        // GET: DocumentRecords/Details/5
+        public async Task<IActionResult> Details(string id, string partition)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
 
-//            var DocumentRecords = await _context.DocumentRecords
-//                .FirstOrDefaultAsync(m => m.Id == id);
-//            if (DocumentRecords == null)
-//            {
-//                return NotFound();
-//            }
+            var DocumentRecords = await _context.DocumentRecords
+                .FirstOrDefaultAsync(m => m.Id == id && m.Partition == partition);
+            if (DocumentRecords == null)
+            {
+                return NotFound();
+            }
 
-//            return View(DocumentRecords);
-//        }
+            return View(DocumentRecords);
+        }
 
-//        // GET: DocumentRecords/Create
-//        public IActionResult Create()
-//        {
-//            return View();
-//        }
+        // GET: DocumentRecords/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
 
-//        // POST: DocumentRecords/Create
-//        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-//        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-//        [HttpPost]
-//        [ValidateAntiForgeryToken]
-//        public async Task<IActionResult> Create([Bind("CsFiles,TsFile,Id")] DocumentRecords DocumentRecords)
-//        {
-//            if (ModelState.IsValid)
-//            {
-//                _context.Add(DocumentRecords);
-//                await _context.SaveChangesAsync();
-//                return RedirectToAction(nameof(Index));
-//            }
-//            return View(DocumentRecords);
-//        }
+        // GET: DocumentRecords/Edit/5
+        public async Task<IActionResult> Edit(string id,  string partition)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
 
-//        // GET: DocumentRecords/Edit/5
-//        public async Task<IActionResult> Edit(int? id)
-//        {
-//            if (id == null)
-//            {
-//                return NotFound();
-//            }
-
-//            var DocumentRecords = await _context.DocumentRecords.FindAsync(id);
-//            if (DocumentRecords == null)
-//            {
-//                return NotFound();
-//            }
-//            return View(DocumentRecords);
-//        }
-
-//        // POST: DocumentRecords/Edit/5
-//        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-//        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-//        [HttpPost]
-//        [ValidateAntiForgeryToken]
-//        public async Task<IActionResult> Edit(int id, [Bind("CsFiles,TsFile,Id")] DocumentRecords DocumentRecords)
-//        {
-//            if (id != DocumentRecords.Id)
-//            {
-//                return NotFound();
-//            }
-
-//            if (ModelState.IsValid)
-//            {
-//                try
-//                {
-//                    _context.Update(DocumentRecords);
-//                    await _context.SaveChangesAsync();
-//                }
-//                catch (DbUpdateConcurrencyException)
-//                {
-//                    if (!DocumentRecordsExists(DocumentRecords.Id))
-//                    {
-//                        return NotFound();
-//                    }
-//                    else
-//                    {
-//                        throw;
-//                    }
-//                }
-//                return RedirectToAction(nameof(Index));
-//            }
-//            return View(DocumentRecords);
-//        }
-
-//        // GET: DocumentRecords/Delete/5
-//        public async Task<IActionResult> Delete(int? id)
-//        {
-//            if (id == null)
-//            {
-//                return NotFound();
-//            }
-
-//            var DocumentRecords = await _context.DocumentRecords
-//                .FirstOrDefaultAsync(m => m.Id == id);
-//            if (DocumentRecords == null)
-//            {
-//                return NotFound();
-//            }
-
-//            return View(DocumentRecords);
-//        }
-
-//        // POST: DocumentRecords/Delete/5
-//        [HttpPost, ActionName("Delete")]
-//        [ValidateAntiForgeryToken]
-//        public async Task<IActionResult> DeleteConfirmed(int id)
-//        {
-//            var DocumentRecords = await _context.DocumentRecords.FindAsync(id);
-//            _context.DocumentRecords.Remove(DocumentRecords);
-//            await _context.SaveChangesAsync();
-//            return RedirectToAction(nameof(Index));
-//        }
-
-//        private bool DocumentRecordsExists(int id)
-//        {
-//            return _context.DocumentRecords.Any(e => e.Id == id);
-//        }
-//    }
-//}
+            var DocumentRecords = await _context.DocumentRecords.FindAsync(id, partition);
+            if (DocumentRecords == null)
+            {
+                return NotFound();
+            }
+            return View(DocumentRecords);
+        }
+    }
+}
